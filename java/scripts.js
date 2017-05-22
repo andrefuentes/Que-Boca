96b259223fbc8c16015b944e80bed11a909a0b2d
andrefuentes <andrefuentes166@gmail.com>
2017-05-22 09:03:50 -0600

    Revert "uyhfgv"
    This reverts commit dd982910db72d670e4bffea123f3f5b7d35351fb.

--- java/scripts.js
+++ java/scripts.js
@@ -16,26 +16,5 @@ var btnSaludar= document.getElementById("saludar");
 
 btnSaludar.onclick =function(){
 	var txtNombre= nombre.value;
-	titulo.innerHTML= "Hola " + txtNombre;
-}
-
-var inAutomatico = document.getElementById("automatico");
-var txAutomatico = document.getElementById("texto-automatico");
-
-inAutomatico.onkeyup = function() {
-	txAutomatico.innerHTML = inAutomatico.value;
-}
-
-var opcion = document.getElementById("website");
-var ir = document.getElementById("ir");
-
-ir.onclick = function(){
-	window.location = opcion.value;	
-}
-
-var square = document.getElementById("square");
-var btnAnimar =document.getElementById("animar");
-
-btnAnimar.onclick= function(){
-	square.classList.add("open");
+	titulo.innerHTML= " Hola " + txtNombre;
 }
