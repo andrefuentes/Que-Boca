e08b572749683c0d4401bc8f6e03c1695ae8aef7
ricardojuarez <ricardojuarez1999@gmail.com>
2017-06-14 08:47:38 -0600

    2.0
    jajaja

--- avatar/models.py
+++ avatar/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Avatares(models.Model) :
-	Photo= models.imagefield
+	Photo= models.ImageField()
 	Character= models.CharField(max_length=100)
